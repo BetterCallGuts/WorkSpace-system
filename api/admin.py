f11f85e075001104dbe493e4e705be58b0f414e5
Omar Hosny <bettercallguts@gmail.com>
2023-11-02 07:45:51 +0200

    Working on it

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models        import Peaple, Recp, Ticket, Token, Packages
 
+def printhello(modeladmin, req, selected):
+
+  # print("Hello world")
+  for i in selected:
+    print(i.name)  
+  
+  
+  
+class InLineTicket(admin.StackedInline):
+  model = Ticket
+  
+  
+
+
 class PeapleAdminStyle(admin.ModelAdmin):
+  inlines      = (InLineTicket,)
   list_display = ("name", "tickets", "he_debt",  "have_debt" )
   search_fields= ("name",)
   list_filter  = ("have_debt",)
@@ -10,20 +25,27 @@ class PeapleAdminStyle(admin.ModelAdmin):
   # actions = ("they_have_debt", )
   
 class TicketAdminStyle(admin.ModelAdmin):
-  list_display  = ( "name_of_ticket", "the_person", "ticket_price", "he_paid" , "still_have", "have_debt","time_added")
-  search_fields = ( "name_of_ticket", "the_person__name", "ticket_price", )
-  list_filter   = ( "have_debt",)
+  # inlines            = (InLinePeaple, )
+  list_display       = ( "name_of_ticket", "the_person", "ticket_price", "he_paid" , "still_have", "have_debt","time_added")
+  search_fields      = ( "name_of_ticket", "the_person__name", "ticket_price", )
+  list_filter        = ( "have_debt",)
   list_display_links = ("time_added",)
-  list_editable = ("name_of_ticket", "ticket_price", "he_paid")
+  list_editable      = ("name_of_ticket", "ticket_price", "he_paid")
+
+
 
 class RecpAdminStyle(admin.ModelAdmin):
+  # inlines            = (InLineRecp,)
   list_display       = ("name", "cost_or_price", "how_much_sold","avilable", "more")
-  fields             = ("image_tag", "name" )
+  fields             = ("image_tag", "name", "image", "cost_or_price","how_much_sold", "avilable" )
   search_fields      = ("cost_or_price", "name", "how_much_sold") 
   list_display_links = ("more", ) 
-  readonly_fields = ('image_tag',)
+  readonly_fields    = ('image_tag',)
   list_filter        = ("avilable",)
   list_editable      = ("name", "cost_or_price", "how_much_sold")
+  
+
+  # actions =[printhello]
 
 
 def rinadmin():
