bce54f053865bd15325993bf041abf0f1572d818
Omar Hosny <bettercallguts@gmail.com>
2023-11-12 19:17:29 +0200

    Cleaned the code a bit

--- Thoth/admin.py
+++ Thoth/admin.py
@@ -1,56 +1,83 @@
-from django.contrib import admin
-from .models        import ( Recp,
- Token, Course, 
-Client, Cafe, Employee, vacation,
-Absent, Deduction,Reward,
- Instructors
-
-)
-from django.contrib import messages
+from django.contrib                import admin
+from django.contrib                import messages
 from django.views.decorators.cache import never_cache
-from django.utils.html import mark_safe
-from django.contrib.auth.models import User, Group
-from django.contrib.auth.admin import UserAdmin, GroupAdmin
-from django.conf import settings
-from django.conf.urls.static import static
-from config.models import CourseType
+from django.utils.html             import mark_safe
+from django.contrib.auth.models    import User, Group
+from django.contrib.auth.admin     import UserAdmin, GroupAdmin
+from django.conf                   import settings
+from django.conf.urls.static       import static
+from config.models                 import CourseType
+from .models                       import (
+Coffee,   Token, 
+Course,   Client,
+Employee, vacation,
+Absent,   Deduction, 
+Reward,   Instructors,
+)
 
+
+# The Admin site 
+# __________________
+class WithAlertAdminPage(admin.sites.AdminSite):
+    # Dash board html page
+    @never_cache
+    def index(self, request, extra_context=None):
+        students =  Client.objects.all()
+        ppl_with_debt = []
+        for i in students:
+          if i.still_have_to_pay() == "He is Clear":
+            continue
+          ppl_with_debt.append(i)
+        if len(ppl_with_debt) ==0:
+          pass
+        else:
+          messages.add_message(request,
+                messages.WARNING  ,
+                mark_safe(f"You have {len(ppl_with_debt)} clients with debt <a href='Thoth/client/?have_debt__exact=1'>click here</a> to see them")
+                )
+        return super(WithAlertAdminPage, self).index(request, extra_context,)
+
+
+# __
+final_boss = WithAlertAdminPage()
+# __
+
+
+
+
+
+# Custom Filters
+# _____________________________
 class FilterClinetsByCourseType(admin.SimpleListFilter):
   title = ('Course type')
   parameter_name = 'course_type'
   def lookups(self, request, model_admin):
     i = CourseType.objects.all()
     x = ( (s.Name, f"{s.Name}")  for s in i )
-    
     return x
-  
+  # 
   def queryset(self, request, queryset):
     list_that_will_be_returned = []
     if self.value():
-      # true
         for i in queryset:
           for c in i.courses.all():
             if c.coursetype.Name == self.value():
               list_that_will_be_returned.append(i.id)
               break
         return queryset.filter(id__in=list_that_will_be_returned)
-    
-
+# _____________________________
 class FilterClinetsByIntructors(admin.SimpleListFilter):
   title = ('instructor')
   parameter_name = 'instructor'
-
+  # 
   def lookups(self, request, model_admin):
     i = Instructors.objects.all()
     x = ( (s.name, f"{s.name}")  for s in i )
     
     return x
-
-
+  # 
   def queryset(self, request, queryset):
     list_that_will_be_returned = []
-
-
     if self.value():
       # true
         for i in queryset:
@@ -59,17 +86,16 @@ class FilterClinetsByIntructors(admin.SimpleListFilter):
             if c.Instructor.name  == self.value():
               list_that_will_be_returned.append(i.id)
               break
-            
         return queryset.filter(id__in=list_that_will_be_returned)
-    
-# class CoursesInLine(admin.StackedInline):
-#   model = Client.courses.through
-  # fields = ("Client__courses__coursetype", )
-  # fields = (Client.courses.field.name,)
+
+
+
+
+
+# Stacked INline
+# _______________________________________
 class VacationInLine(admin.StackedInline):
   model = vacation
-  # fields = ("Client__courses__coursetype", )
-  # fields = (Client.courses.field.name,)
 
 class DeductionInLine(admin.StackedInline):
   model = Deduction
@@ -79,37 +105,18 @@ class AbsentInLine(admin.StackedInline):
   
 class RewardInLine(admin.StackedInline):
   model = Reward
-  
-# class LevelsInLine(admin.StackedInline):
-#   model = Level
-# class StillHaveToPay(admin.SimpleListFilter):
-#   title = ('Have debt?')
-#   parameter_name = 'decade'
 
-#   def lookups(self, request, model_admin):
-    
-#     return (
-#         (True, ('Yes')),
-#         (False, ('No')),
-#     )
-#   def queryset(self, request, queryset):
-#     list_that_will_be_returned = []
-#     if self.value():
-#       # true
-#         for i in queryset:
-#           if i.still_have_to_pay == "He is Clear":
-#             pass
-#           else:
-#             list_that_will_be_returned.append(i)
-#         return list_that_will_be_returned
-#     if not self.value() :
-#       # false
-#         for i in queryset:
-#           if i.still_have_to_pay == "He is Clear":
-#             list_that_will_be_returned.append(i)
-#           else:
-#             pass
-#         return list_that_will_be_returned
+
+
+
+
+
+
+
+
+# ModelAdmins Custom admin site
+# ____________________________-
+
 
 class ClientAdmin(admin.ModelAdmin):
   fields       = ("name", "phone_number",
@@ -131,17 +138,13 @@ class ClientAdmin(admin.ModelAdmin):
     "name",
     "phone_number",
     "paid",
-    # "courses_in"
     )
   list_display_links = ("more",)
   list_filter        = ('have_debt', 
         FilterClinetsByCourseType,
         FilterClinetsByIntructors
-
-
         )
-  # list_editable = 
-
+# ________________
 class EmpAdmin(admin.ModelAdmin):
   inlines  = (
     VacationInLine,
@@ -177,149 +180,36 @@ class EmpAdmin(admin.ModelAdmin):
         "job_postition","Date_of_join",
         
         )
-  # list_editable = ("more",)
   list_display_links = ("more",)
   list_filter = ("state_of_marrieg", )
   search_fields = ("name", "cur_sallary")
   readonly_fields    = ('image_tag',)
-
+# ___________________
 class InstructorsAdminStyle(admin.ModelAdmin):
-  
-  list_display = ("name", "phone_number","specialities")
+  list_display = (
+    "name", "phone_number",
+    "specialities"
+    )
   list_filter  = ("specialty", )
-
-
-
+# ________________________
 class CourseAdminStyle(admin.ModelAdmin):
   list_display = ("coursetype", "Instructor","start_date", "end_date", "course_levels")
-  # inlines      = (CoursesInLine, )
-
-class WithAlertAdminPage(admin.sites.AdminSite):
-
-
-    # def main(self):
-    #   pple = Peaple.objects.all()
-    #   tickets =  Ticket.objects.all()
-    #   total = 0
-    #   for i in pple:
-    #     if i.is_clear():
-    #       # print("he is clear")
-    #       pass
-    #     else:
-    #       # print("h")
-    #       total += 1
-      
-    #   return tickets, total
-
-      # def get_urls(self):
-        
-      #   urlpatterns = self.super().get_urls()
-      #   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-      #   return urlpatterns
-
-    @never_cache
-    def index(self, request, extra_context=None):
-
-        students =  Client.objects.all()
-        ppl_with_debt = []
-
-        for i in students:
-          if i.still_have_to_pay() == "He is Clear":
-            continue
-          ppl_with_debt.append(i)
-        
-        if len(ppl_with_debt) ==0:
-          pass
-        else:
-          messages.add_message(request,
-                messages.WARNING  ,
-                mark_safe(f"You have {len(ppl_with_debt)} clients with debt <a href='Thoth/client/?have_debt__exact=1'>click here</a> to see them")
-                )
-
-        # if total == 0:
-          
-        #   messages.add_message(request, messages.SUCCESS,mark_safe( f'You have no debtor !!, check out your finished {len(tickets)} tickets <a href="/api/ticket/"> here </a>'))
-        #   return super().index(request, extra_context)
-
-        # # 
-        # messages.add_message(request, messages.WARNING, mark_safe( f'There is {total} people with debt <a href="/api/peaple/?have_debt__exact=1" > check them </a> out, And {len(tickets)} Tikets Click <a href="/api/ticket/">here to see them</a>'))
-        # x = Employee.objects.all()
-        # for i in x:
-        #   # print(i.img.url)
-        #   pass
-        return super(WithAlertAdminPage, self).index(request, extra_context,)
-          # print(i.is_clear())
-
-
-final_boss = WithAlertAdminPage()
-# final_boss.urls
-final_boss.register(User, UserAdmin)
-final_boss.register(Group, GroupAdmin)
-final_boss.register(Client,ClientAdmin)
-final_boss.register(Course, CourseAdminStyle)
-# final_boss.register(CourseType)
-final_boss.register(Employee, EmpAdmin)
-# final_boss.register(Level)
-final_boss.register(Instructors, InstructorsAdminStyle)
-# final_boss.site_title = "Thoth"
-# final_boss.site_header = "Thoth"
-# final_boss.index_title = "Dash Board"
-
-
-
-
-
-
-
-# def printhello(modeladmin, req, selected):
-
-  # # print("Hello world")
-  # for i in selected:
-  #   print(i.name)  
-  
-# admin.site.index_template = "my_index.html"
-# class CustomAdminSite(admin.):
-#     @never_cache
-#     def index(self, request, extra_context=None):
-#         messages.add_message(request, messages.INFO, 'This is a message.')
-#         return super().index(request, extra_context)
-  
-
-
-# admin_site = CustomAdminSite(name='myadmin')
-
-# class InLineTicket(admin.StackedInline):
-#   model = Ticket
-  
-  
-
-
+# _____________________________
 class PeapleAdminStyle(admin.ModelAdmin):
-  # inlines      = (InLineTicket,)
   list_display = ("name", "tickets", "he_debt",  "have_debt" )
   search_fields= ("name",)
   list_filter  = ("have_debt",)
   list_display_links = ("tickets",)
   list_editable= ("name", )
-  # actions = ("they_have_debt", )
-  # def save_model(self, request, obj, form, change):
-        
-  #       messages.add_message(request, messages.INFO, 'Car has been sold')
-  #       super(PeapleAdminStyle, self).save_model(request, obj, form, change)
-  
+# _____________________________
 class TicketAdminStyle(admin.ModelAdmin):
-  # inlines            = (InLinePeaple, )
   list_display       = ( "name_of_ticket", "the_person", "ticket_price", "he_paid" , "still_have", "have_debt","time_added")
   search_fields      = ( "name_of_ticket", "the_person__name", "ticket_price", )
   list_filter        = ( "have_debt",)
   list_display_links = ("time_added",)
   list_editable      = ("name_of_ticket", "ticket_price", "he_paid")
-
-
-
-class RecpAdminStyle(admin.ModelAdmin):
-  # inlines            = (InLineRecp,)
+# _______________________________
+class CoffeeAdminStyle(admin.ModelAdmin):
   list_display       = ("name", "cost_or_price", "how_much_sold","avilable", "more")
   fields             = ("image_tag", "name", "image", "cost_or_price","how_much_sold", "avilable" )
   search_fields      = ("cost_or_price", "name", "how_much_sold") 
@@ -327,67 +217,14 @@ class RecpAdminStyle(admin.ModelAdmin):
   readonly_fields    = ('image_tag',)
   list_filter        = ("avilable",)
   list_editable      = ("name", "cost_or_price", "how_much_sold")
-  
-
-  # actions =[printhello] #i commented the function
 
 
-# def rinadmin():
-#   x = Packages.objects.first()
-#   try:
-#     if x.Peaple:
-#       final_boss.register(Peaple, PeapleAdminStyle)
-    
-#   except:
-#     pass
-#   try:
-      
-#       if not x.Peaple:
-#         final_boss.unregister(Peaple)
-#   except:
-#       pass
-#   try:
-    
-#     if x.Recption:
-#       final_boss.register(Recp,   RecpAdminStyle  )
-#   except:
-#     pass
-#   try:
-    
-#     if not x.Recption:
-#       final_boss.unregister(Recp)
-#   except:
-#     pass
-#   try:
-    
-#     if x.Ticket:
-#       final_boss.register(Ticket, TicketAdminStyle)
-#   except: pass
-#   try:
-#     if not x.Ticket:
-      
-#       final_boss.unregister(Ticket)
-#   except:pass
-#   try:
-    
-#     if x.Token:
-#       final_boss.register(Token)
-#   except:
-#     pass
-#   try:  
-      
-#     if not x.Token:
-#       final_boss.unregister(Token)
-#   except:
-#     pass
+# Register models
 
-# rinadmin()
-
-
-# final_boss.register(Packages)
-
-
-# final_boss.register(Peaple, PeapleAdminStyle)
-# final_boss.register(Recp,  RecpAdminStyle)
-# final_boss.register(Ticket, TicketAdminStyle)
-final_boss.register(Recp, RecpAdminStyle)
+final_boss.register(User, UserAdmin)
+final_boss.register(Group, GroupAdmin)
+final_boss.register(Client,ClientAdmin)
+final_boss.register(Course, CourseAdminStyle)
+final_boss.register(Employee, EmpAdmin)
+final_boss.register(Instructors, InstructorsAdminStyle)
+final_boss.register(Coffee, CoffeeAdminStyle)
--- Thoth/models.py
+++ Thoth/models.py
@@ -1,254 +1,92 @@
-from django.db import models
-from phone_field import PhoneField
-from django import forms
-import datetime
+from django.db   import           models
+from phone_field import           PhoneField
+from django      import           forms
+import                            datetime
 from django.utils.timezone import now
-# Create your models here.
-from django.contrib import admin
-import uuid
-import os
-from django.utils.html import mark_safe
-from config.models import JobPosition
-# from datetimewidget.widgets import DateTimeWidget
+from django.contrib import        admin
+import                            uuid
+import                            os
+from django.utils.html import     mark_safe
+from config.models import         JobPosition
 
+# GLobal Vars
 app_label = "Thoth"
-
-# print("****"*10)
 HOST = os.environ.get("HOST", "http://127.0.0.1:8000/")
-# print(HOST)
-# print("****"*10)
-class Recp(models.Model):
 
+# Models
+# ________________________
+class Token(models.Model):
+  # 
+  token = models.UUIDField(default=uuid.uuid1, editable=False)
+  # 
+  def __str__(self):
+    return f"{self.token}"
 
+class Coffee(models.Model):
+  # 
   name          = models.CharField(max_length=255 )
   image         = models.ImageField()
   cost_or_price = models.IntegerField(help_text="Price")
   how_much_sold = models.IntegerField( default=0) 
   avilable      = models.BooleanField(default=True)
   more          = models.CharField(default="More", max_length=255, editable=False)
+  # 
   def __str__(self):
-    
-    
     return f"{self.name}"
+  # 
   def image_tag(self):
     return mark_safe(f'<img style="width:200px;hieght:200px" src="{HOST}/media/{ self.image}" />' )
   image_tag.short_description = 'Image'
   image_tag.allow_tags = True
+  # 
   class Meta:
         app_label = app_label
-        # abstract = True
   
 
 
 
-
-
-
-
-# class Peaple(models.Model):
-#   name = models.CharField(max_length=255)
-#   have_debt = models.BooleanField(default=False)
-  
-#   def tickets(self):
-#     person_ticket = Ticket.objects.filter(the_person__name=self.name)
-#     person_ticket = [x.name_of_ticket for x in person_ticket]
-#     if len(person_ticket) == 0:
-#       return "He doesnt have any ticket yet"
-    
-#     return person_ticket
-#   class Meta:
-#         app_label = app_label
-#         # abstract = True
-#   def is_clear(self):
-#     person_ticket = Ticket.objects.filter(the_person__name=self.name)
-#     person_ticket = [x for x in person_ticket]
-#     if len(person_ticket) == 0:
-#       return True
-    
-#     succ = []
-#     for i in person_ticket:
-#       if i.he_paid == i.ticket_price:
-#         succ.append(True)
-#         continue
-#       succ.append(False)
-    
-#     if False in succ:
-#       return False
-#     return True
-    
-#   def he_debt(self):
-#     person_ticket = Ticket.objects.filter(the_person__name=self.name)
-#     person_ticket = [x for x in person_ticket]
-#     result = 0
-#     for i in person_ticket:
-#       result += i.still_have()
-#     return result
-
-#   def __str__(self):
-#     return f"{self.name}"
-  
-#   def they_have_debt(self):
-#     pass
-  
-#   def save(self):
-#     super().save()
-#     person_ticket = Ticket.objects.filter(the_person__name=self.name)
-#     person_ticket = [x for x in person_ticket]
-#     result = 0
-#     for i in person_ticket:
-#       result += i.still_have()
-#     if result == 0:
-#       # print("wtf is this")
-#       self.have_debt = False
-
-#     else:
-#       self.have_debt = True
-#       # print("wtf is this Twice")
-
-#     super().save()
-
-class Token(models.Model):
-  token = models.UUIDField(default=uuid.uuid1, editable=False)
-
-  def __str__(self):
-    # print(self.check())
-    
-    
-    return f"{self.token}"
-
-# class PeapleAdminStyle(admin.ModelAdmin):
-#   list_display = ("name", "tickets", "he_debt",  "have_debt" )
-#   search_fields= ("name",)
-#   list_filter  = ("have_debt",)
-#   list_display_links = ("tickets",)
-#   list_editable= ("name", )
-#   # actions = ("they_have_debt", )
-  
-# class TicketAdminStyle(admin.ModelAdmin):
-#   list_display  = ( "name_of_ticket", "the_person", "ticket_price", "he_paid" , "still_have", "have_debt","time_added")
-#   search_fields = ( "name_of_ticket", "the_person__name", "ticket_price", )
-#   list_filter   = ( "have_debt",)
-#   list_display_links = ("time_added",)
-#   list_editable = ("name_of_ticket", "ticket_price", "he_paid")
-
-# class RecpAdminStyle(admin.ModelAdmin):
-#   list_display       = ("name", "cost_or_price", "how_much_sold","avilable", "more")
-#   search_fields      = ("cost_or_price", "name", "how_much_sold") 
-#   list_display_links = ("more", ) 
-#   list_filter        = ("avilable",)
-#   list_editable      = ("name", "cost_or_price", "how_much_sold")
-
-
-# def rinadmin():
-#   x = Packages.objects.first()
-#   try:
-#     if x.Peaple:
-#       admin.site.register(Peaple, PeapleAdminStyle)
-    
-#   except:
-#     pass
-#   try:
-      
-#       if not x.Peaple:
-#         admin.site.unregister(Peaple)
-#   except:
-#       pass
-#   try:
-    
-#     if x.Recption:
-#       admin.site.register(Recp,   RecpAdminStyle  )
-#   except:
-#     pass
-#   try:
-    
-#     if not x.Recption:
-#       admin.site.unregister(Recp)
-#   except:
-#     pass
-#   try:
-    
-#     if x.Ticket:
-#       admin.site.register(Ticket, TicketAdminStyle)
-#   except: pass
-#   try:
-#     if not x.Ticket:
-      
-#       admin.site.unregister(Ticket)
-#   except:pass
-#   try:
-    
-#     if x.Token:
-#       admin.site.register(Token)
-#   except:
-#     pass
-#   try:  
-      
-#     if not x.Token:
-#       admin.site.unregister(Token)
-#   except:
-#     pass
-
-
-# class Packages(models.Model):
-  
-#   Recption = models.BooleanField(default=True)
-#   Peaple   = models.BooleanField(default=True)
-#   Ticket   = models.BooleanField(default=True)
-#   Token    = models.BooleanField(default=True)
-  
-#   def __str__(slef):
-#     return f"Package manager Don't Touch!!"
-  
-#   def save(self):
-#     super().save()
-#     rinadmin()
-
-# #     super().save()
-
-#   class Meta:
-#         app_label = app_label
-#         # abstract = True
 
 
 
 class Course(models.Model):
+  # 
   coursetype = models.ForeignKey("config.CourseType", on_delete=models.CASCADE)
   start_date = models.DateField(default=datetime.datetime.now)
   levels     = models.ManyToManyField("config.Level", related_name="levels", blank=True)
   Instructor = models.ForeignKey("Instructors", on_delete=models.CASCADE, null=True, blank=True)
   end_date   = models.DateField(default=datetime.datetime.now)
   cost_forone= models.FloatField(blank=True, default=0, verbose_name="Course cost per person")
-
+  # 
   def __str__(self):
     return f"{self.coursetype}|start:{self.start_date}|end:{self.end_date}"
+  # 
   class Meta:
       app_label = app_label
-      # abstract = True
+  # 
   def course_levels(self):
     levels_many_to_many = self.levels.all()
-    
     return ", ".join([x.Level for x in levels_many_to_many])
 
-    
 
 class Client(models.Model):
-  
+  # 
   name           = models.CharField(max_length=255)
   courses        = models.ManyToManyField(Course,  blank=True)
-  
   phone_number   = models.CharField(max_length=255)
   birth_day      = models.DateField(default=datetime.datetime.now)
   paid           = models.FloatField(default=0, blank=True, verbose_name="Paid")
   have_debt      = models.BooleanField(default=True, )
-  
+  # 
   def total(self):
     cost  = self.courses.all()
     result= 0
     for i in cost:
       result += i.cost_forone
     return result
+  # 
   def more(self):
     return "More"
+  # 
   def courses_in(self):
     x = self.courses.all()
     courses_type = []
@@ -257,46 +95,65 @@ class Client(models.Model):
     if len(courses_type) == 0:
       return "He is not in course"
     return ", ".join(courses_type)
+  # 
   def still_have_to_pay(self):
-    # print("*"*100)
-    # print(type(self.total))
     still_didt_pay = float(self.total()) - float(self.paid)
     if still_didt_pay == 0:
       return "He is Clear"
     return still_didt_pay
+  # 
   def __str__(self):
     return f"{self.name}"
+  # 
   def save(self):
     if self.still_have_to_pay() == "He is Clear":
       self.have_debt = False
     else:
       self.have_debt = True
-    
     super().save()
+  # 
   class Meta:
+    app_label = app_label
 
 
-        app_label = app_label
-        # abstract = True
-  
-# 
-
-class Cafe(models.Model):
-  class Meta:
-        app_label = app_label
-        # abstract = True
 
 class Service(models.Model):
-  
+  # 
 
+  # 
   class Meta:
-          app_label = app_label
-          # abstract = True
+    app_label = app_label
 
 
-# Camera type tapo
+# 
+class Instructors(models.Model):
+  # 
+  name         = models.CharField(max_length=255)
+  phone_number = models.CharField(max_length=255, blank=True, null=True)
+  specialty    = models.ManyToManyField("config.CourseType",related_name="speciality",blank=True)
+  # 
+  def __str__(self):
+    return f"{self.name}"
+  # 
+  def specialities(self):
+    data = self.specialty.all()
+    verbose_name = "lol"
+    s = []
+    for i in data:
+      s.append(i.Name)
+    if len(s) == 0:
+      return "Empty"
+    return " ,".join(s)
+  # 
+  class Meta:
+    verbose_name = "Instructor"
+
+
+# Employee section
+# _____________________________________
 
 class vacation(models.Model):
+  # 
   choices = (
     ("Sick leave", "Sick leave"),
     ("weekends", "weekends")    ,
@@ -305,57 +162,56 @@ class vacation(models.Model):
   Emp           = models.ForeignKey("Employee", on_delete=models.CASCADE)
   vacation_type = models.CharField(choices=choices, max_length=300) 
   how_many_days = models.IntegerField(verbose_name="how many days",default=0 )
+  # 
   class Meta:
         app_label = app_label
-        # abstract = True
-  
+  # 
   def __str__(self):
     return f"{self.vacation_type}|{self.how_many_days}"
   
 class Absent(models.Model):
-  
+  # 
   Emp           = models.ForeignKey("Employee", on_delete=models.CASCADE)
   how_many_days = models.IntegerField(verbose_name="how many days") 
-  
+  # 
   def __str__(self):
-    
     return f"{self.how_many_days} day"
+  # 
   class Meta:
-        app_label = app_label
+    app_label = app_label
   
 
 class Deduction(models.Model):
-  
+  # 
   Emp        = models.ForeignKey("Employee", on_delete=models.CASCADE)
   The_amount = models.IntegerField(verbose_name="Amount")
   the_reson  = models.TextField(null=True, blank=True, verbose_name="reason")
-
-
+  # 
   class Meta:
     app_label = app_label
+  # 
   def __str__(self):
-    
     return f"{self.The_amount}"
 
 
 class Reward(models.Model):
-  
+  # 
   Emp        = models.ForeignKey("Employee", on_delete=models.CASCADE)
   The_amount = models.IntegerField(verbose_name="Amount")
   the_reson  = models.TextField(null=True, blank=True, verbose_name="reason")
   img        = models.ImageField(verbose_name="Picture if there certificate[optional]", blank=True)
-
-
+  # 
   class Meta:
     app_label = app_label
+  # 
   def __str__(self):
-    
     return f"{self.The_amount}"
 
 
 
 
 class Employee(models.Model):
+  # 
   choices = (
     ("Married" , "Married" ),
     ("Single"  , "Single")  ,
@@ -364,7 +220,7 @@ class Employee(models.Model):
   )
   name             = models.CharField(max_length=255)
   Person_identf    = models.CharField(max_length=255, default=" ",verbose_name="ID", null=True, blank=True) 
-  EDU_state        = models.CharField(max_length=300, default=" ", null=True, blank=True, verbose_name="	Educational Level") 
+  EDU_state        = models.CharField(max_length=300, default=" ", null=True, blank=True, verbose_name="Educational Level") 
   address          = models.CharField(max_length=300, default=" ", null=True, blank=True) 
   img              = models.ImageField(upload_to="EMP Pic", verbose_name=" ", blank=True)
   cur_sallary      = models.FloatField(verbose_name="Current Salary") 
@@ -374,58 +230,16 @@ class Employee(models.Model):
   phone_number_eme = models.CharField(max_length=20, verbose_name="Emergency phone number", blank=True, null=True)
   job_postition    = models.ForeignKey(JobPosition, null=True, on_delete=models.SET_NULL, blank=True)
   more             = models.CharField(editable=False, default="More",max_length=10)
-
-  
-
+  # 
   def image_tag(self):
     return mark_safe(f'<img style="width:200px;hieght:200px" src="{HOST[:-1]}{ self.img.url}" />' )
+  # 
+  def __str__(self):
+    return f"{self.name}"
+  # 
+  class Meta:
+    app_label = app_label
+  
   image_tag.short_description = 'Image'
   image_tag.allow_tags = True
 
-
-  def __str__(self):
-    return f"{self.name}"
-  class Meta:
-        app_label = app_label
-        # abstract = True
-        
-
-
-  
-  
-  
-
-class Instructors(models.Model):
-  
-  name         = models.CharField(max_length=255)
-  phone_number = models.CharField(max_length=255, blank=True, null=True)
-  specialty    = models.ManyToManyField("config.CourseType",related_name="speciality",blank=True)
-  # salary = ...
-
-  ''' 
-  name
-  course
-  phone number
-
-  '''
-  
-  def __str__(self):
-    return f"{self.name}"
-  def specialities(self):
-    data = self.specialty.all()
-    verbose_name = "lol"
-    s = []
-    for i in data:
-      # print(i.description)
-      s.append(i.Name)
-    # self.play = "yea body !!!"
-    # print(data)
-    # print("********"*10)
-    if len(s) == 0:
-      return "Empty"
-    return " ,".join(s)
-  
-  class Meta:
-    app_label = app_label
-    verbose_name = "Instructor"
-  
