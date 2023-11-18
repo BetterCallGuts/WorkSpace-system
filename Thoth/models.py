7523fa4853ce1aa48e44c894fd3709048ec5ce89
Omar Hosny <bettercallguts@gmail.com>
2023-11-18 19:48:18 +0200

    Removed the phone number field

--- Thoth/models.py
+++ Thoth/models.py
@@ -1,5 +1,4 @@
 from django.db   import           models
-from phone_field import           PhoneField
 from django      import           forms
 import                            datetime
 from django.utils.timezone import now
